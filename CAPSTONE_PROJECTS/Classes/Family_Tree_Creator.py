75e4009d36a086b24dee986a059377dda5dbfa9f
Gustavo Lage <gustavo.lage.dev@gmail.com>
2024-10-22 17:12:47 -0300

    Adding complexity to Family_tree_creator project

--- CAPSTONE_PROJECTS/Classes/Family_Tree_Creator.py
+++ CAPSTONE_PROJECTS/Classes/Family_Tree_Creator.py
@@ -1,6 +1,10 @@
 #**Family Tree Creator** - Create a class called Person which will have a name, when they were born and when (and if) they died. 
 #Allow the user to create these Person classes and put them into a family tree structure. Print out the tree to the screen. 
 
+#FEATURES TO BE IMPLEMENTED: 
+# - CHILDREN RIGHT BELOW PARENTS 
+# - FROM OLDER TO YOUNGER 
+# - ID WHO IS CHILD OF WHO
 class Person:
 
 	def __init__(self,name,birth_date,parents=(),spouse=""):
@@ -16,6 +20,8 @@ class Person:
 
 		if parents != ():
 			p_gen = max(parents[0].family_gen,parents[1].family_gen)
+			self.parents[0].spouse = self.parents[1]
+			self.parents[1].spouse = self.parents[0]
 			for p in parents:
 				p.children.append(self)
 				p.family_gen = p_gen
@@ -23,20 +29,10 @@ class Person:
 
 		family_list.append(self)
 
-	def add_child(self,*args): # Object Person is passed as a new child
-		for child in args:
-			self.children.append(child)
-			child.parents.append(self)
+def id_parents(person):
+	pass
 
-			if self.family_gen == None:
-				if self.parents == ():
-					self.family_gen = 1
-				else:
-					self.family_gen = self.parents[0].family_gen + 1
-
-			child.family_gen = self.family_gen + 1
-
-def check_gen(family_list,gen): # Check if there isn't nobody on the actual generation
+def check_gen(family_list,gen): # Check if there isn't anyone on the actual generation
 
 	for p in family_list:
 		if p.family_gen == gen:
@@ -45,43 +41,42 @@ def check_gen(family_list,gen): # Check if there isn't nobody on the actual gene
 	return gen + 1
 
 def display_tree(family_list):
-
 	gen = 1
 	print(30*" " + "This is our Family Tree!")
 	print(f"\nGeneration {gen}:", end="")
 	while family_list != []:
 		gen = check_gen(family_list,gen)
-		for person in family_list:
-			if person.family_gen == gen: # Generation order
-				if parents_list != []:
-					if person.parents == parents_list[0]:
-						for child in person.parents[0].children:
-							if child.children != []:
-								couple = child.children[0].parents
-								print(f"\t{couple[0].name} S2 {couple[1].name}\t", end="")
-								family_list.remove(couple[0])
-								family_list.remove(couple[1])
-								parents_list.append(couple)
-							else:
-								print("\t" + child.name, end="")
-								family_list.remove(child)
-						print("\t", end="")
-						parents_list.remove(person.parents)
-				elif person.children != []:
-					couple = person.children[0].parents
-					print(f"\t{couple[0].name} S2 {couple[1].name}\t", end="")
+		singles_childs_list = []
+		if children_list != []:
+			for child in children_list[:]:
+				if child.children != []:
+					print(f"\t{child.name} S2 {child.spouse.name}\t", end="")
 					family_list.remove(couple[0])
 					family_list.remove(couple[1])
-					parents_list.append(couple)
+					children_list.remove(child)
+					children_list.extend(child.children)
 				else:
-					print("\t" + person.name, end="")
-					family_list.remove(person)
+					singles_childs_list.append(child)
+			for single in singles_childs_list:
 
+				print("\t" + single.name, end="")
+				print("\t", end="")
+				family_list.remove(single)
+				children_list.remove(single)
+		else:
+			for person in family_list:
+				if person.family_gen == gen: # Generation order
+					if person.children != []:
+						couple = person.children[0].parents
+						print(f"\t{couple[0].name} S2 {couple[1].name}\t", end="")
+						family_list.remove(couple[0])
+						family_list.remove(couple[1])
+						children_list.extend(person.children)
 
 if __name__ == "__main__":
 
 	family_list = []
-	parents_list = []
+	children_list = []
 
 	odin = Person("Odin",(1,1,1))
 	freya = Person("Freya",(2,2,2))
@@ -93,5 +88,6 @@ if __name__ == "__main__":
 	selena = Person("Selena",(14,8,2016),(rafael,laura))
 	cristiano = Person("Cristiano",(5,4,2010),(wesley,alana))
 	ronaldo = Person("Ronaldo",(5,4,2010),(rafael,laura))
-
+	thor = Person("Thor",(3,3,3),(odin,freya))
+	
 	display_tree(family_list)
