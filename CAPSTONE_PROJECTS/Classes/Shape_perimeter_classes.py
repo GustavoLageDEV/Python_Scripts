4e3351076981c5242a2fcec043bcc7d0386ec8e2
Gustavo Lage <gustavo.lage.dev@gmail.com>
2024-12-11 13:43:30 -0300

    WIP mylibrary.py

--- CAPSTONE_PROJECTS/Classes/Shape_perimeter_classes.py
+++ CAPSTONE_PROJECTS/Classes/Shape_perimeter_classes.py
@@ -2,3 +2,23 @@
 # Create an abstract class called Shape and then inherit from it other shapes like diamond, rectangle, circle, triangle etc. 
 # Then have each class override the area and perimeter functionality to handle each shape type.  
 #[[masegaloeh (Python)]](https://github.com/masegaloeh/freetime-projects/blob/master/class/shape/shape.py) 
+
+class Shape:
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+
+class Rectangle(Shape):
+
+    def __init__(self,height,width)
+        
+        self.height = height
+        self.width = width
+        
--- mylibrary.py
+++ mylibrary.py
@@ -16,23 +16,39 @@ cursor = conn.cursor()
 
 def add_user(first_name,last_name,email):
     
-    cursor.execute("INSERT INTO users(first_name,last_name,email) VALUES(%s,%s,%s) RETURNING user_id", (first_name,last_name,email))
+    cursor.execute("INSERT INTO users(first_name,last_name,email,last_login) VALUES(%s,%s,%s,%s) RETURNING user_id", (first_name,last_name,email,datetime.datetime.now()))
     conn.commit()
-    return cursor.fetchone()[0]
+    return print(f" User added successfully. user_id = {cursor.fetchone()[0]}")
 
 def add_book(title,author):
     
     cursor.execute("INSERT INTO books(title,author) VALUES(%s,%s) RETURNING book_id", (title,author))
     conn.commit()
-    return cursor.fetchone()[0]
+    return print(f" Book added successfully. book_id = {cursor.fetchone()[0]}")
 
-def book_loan(user_id,book_id):
+def loan_book(user_id,book_id):
 
-    cursor.execute("SELECT copies_avaiable FROM books WHERE book_id = %s", user_id)
-    cursor.execute("INSERT INTO loans(loan_id) VALUES(%s) RETURNING loan_id", datetime.datetime.now())
+    cursor.execute("SELECT copies_avaiable FROM books WHERE book_id = %s", (book_id,))
+    copies_avaiable = cursor.fetchone()[0]
+    if copies_avaiable == 0:
+        return print("This book has no copies avaiable at this moment")
+    else:
+        cursor.execute("INSERT INTO loans(user_id,book_id,loan_date) VALUES(%s,%s,%s) RETURNING loan_id",(user_id,book_id, datetime.datetime.now()))
+        loan_id = cursor.fetchone()[0]
+        cursor.execute("UPDATE books SET copies_avaiable = copies_avaiable - 1 WHERE book_id = %s",(book_id,))
+        cursor.execute("UPDATE users SET last_login = %s WHERE user_id = %s" , (datetime.datetime.now(),user_id))
+        conn.commit()
+        return print(f"Loan made with loan_id = {loan_id}")
+    
+def return_book(loan_id):
+    cursor.execute("SELECT user_id,book_id FROM loans WHERE loan_id = %s", (loan_id,))
+    data = cursor.fetchone()
+    user_id = data[0]
+    book_id = data[1]
+    cursor.execute("UPDATE users SET last_login = %s WHERE user_id = %s" , (datetime.datetime.now(),user_id))
+    cursor.execute("UPDATE books SET copies_avaiable = copies_avaiable + 1 WHERE book_id = %s" , (book_id,))
+    cursor.execute("UPDATE loans SET return_date = %s WHERE loan_id = %s" , (datetime.datetime.now(),loan_id))
     conn.commit()
-    return cursor.fetchone()[0]
+    return print(f"Book returned successfully")
 
-#add_book("Memórias Póstumas de Brás Cubas","Machado de Assis")
-
-print(cursor.execute("SELECT copies_avaiable FROM books WHERE book_id = 1"))
+return_book(4)
